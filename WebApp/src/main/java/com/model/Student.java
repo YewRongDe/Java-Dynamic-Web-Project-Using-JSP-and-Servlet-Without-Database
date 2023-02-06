976f4be7dda44de5b7f39241d9affc8bfec58996
YewRongDe <37549060+YewRongDe@users.noreply.github.com>
2023-02-06 14:46:41 +0800

    Remove unused code

--- WebApp/src/main/java/com/model/Student.java
+++ WebApp/src/main/java/com/model/Student.java
@@ -8,7 +8,6 @@ public class Student {
 	private String pass;
 	
 	public Student(String department, String studentId, String name, int marks) {
-		super();
 		this.department = department;
 		this.studentId = studentId;
 		this.name = name;
